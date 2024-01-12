e68e8aa20c8d62cd80d1996f469c2a9b9d18a8b4
dulcelene machado <dudutola@gmail.com>
2024-01-12 13:19:36 +0100

    Updated script.js with new content Updated and added some comments Updated the links in const slides
    Created a new function arrowRightClick to handle the moving to the
    next slide when right arrow is clicked
    Added a new img and tagLine variables, to update the infos
    inside of it with: img.src to change the images source to a new
    one, and same thing with tagLine.innerHTML, to change content
    Added a if statement to check if the index surpasses the last
    slide, if true it reaches the end, so resets to 0 again
    Added a new variable called listDots, to select all the .dot
    inside .dots
    Added and removed the class .dot_selected from the listDots index
    to select and unselect the current slide
    
    Created a new function arrowLeftClick to handle the moving to the
    next slide when left arrow is clicked
    Same logic from function arrowRightClick
    Only changed the if statement, in this case if the index is less
    than 0, it reaches the end, so if true, it is send to the last slide

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,106 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log(slides)
 
-// selectionner les elements
-let arrowLeft = document.getElementById('arrowLeft')
-let arrowRight = document.getElementById('arrowRight')
+/** Select the elements.*/
+const arrowLeft = document.getElementById('arrowLeft');
+const arrowRight = document.getElementById('arrowRight');
 
-// add event listener, en gros auront la meme logique mais non
-arrowLeft.addEventListener('click', (event) => {
-  console.log(event)
-})
-
-arrowRight.addEventListener('click', (event) => {
-  console.log(event)
-})
-
-
-// select dots
-const dots = document.querySelector('.dots')
-// select dots index
+const dots = document.querySelector('.dots');
 let index = 0;
 
-// for loop, la logique serait que pour chaque tour, le dot symbolise une image
-// afficher les dots
+const img = document.querySelector('#banner img');
+const tagLine = document.querySelector('#banner p');
+
+
+/** This function handle the logic for moving to the next slide in a slideshow,
+* when left arrow is clicked.*/
+function arrowLeftClick() {
+  arrowLeft.addEventListener('click', (event) => {
+    console.log(event);
+
+    const listDots = document.querySelectorAll('.dots .dot');
+    listDots[index].classList.remove('dot_selected');
+
+    index--;
+
+    /** Checks if the index is less than 0,
+    * it reached the beginning, so if true, sends to the last slide.*/
+    if (index < 0) {
+      index = slides.length - 1;
+    }
+
+    listDots[index].classList.add('dot_selected');
+    img.src = slides[index].image
+    tagLine.innerHTML = slides[index].tagLine
+  })
+}
+/** Calls the function.*/
+arrowLeftClick()
+
+
+/** This function handle the logic for moving to the next slide in a slideshow,
+* when right arrow is clicked.*/
+function arrowRightClick() {
+  arrowRight.addEventListener('click', (event) => {
+    console.log(event);
+
+    /** Selects all the elements with class dot.*/
+    const listDots = document.querySelectorAll('.dots .dot');
+    /** Unselect the actual slide, it removes the class dot_selected.*/
+    listDots[index].classList.remove('dot_selected');
+
+    index++;
+
+    /** Checks if the index surpasses the last slide,
+    * if true, it reached the end, so resets to 0 (index=0).*/
+    if (index > slides.length - 1) {
+      index = 0;
+    }
+    /** Select the actual slide, it adds the class dot_selected.*/
+    listDots[index].classList.add('dot_selected');
+    /** Updates the image source to a new one.*/
+    img.src = slides[index].image
+    /** Updates the HTML content to a new one.*/
+    tagLine.innerHTML = slides[index].tagLine
+  })
+}
+/** Calls the function.*/
+arrowRightClick()
+
+
+/** This function creates and displays the bullets points.*/
 function displayDots() {
   for (let i = 0; i < slides.length; i++) {
-    // creer un element
+    /** Create a new span element(dot).*/
     const dot = document.createElement('span');
-    // add la classe a l'interieur
+    /** Addds the class dot to this dot variable.*/
     dot.classList.add('dot');
-    // on place le dot dans dots
+    /** Addds this dot element to dots variable.*/
     dots.appendChild(dot);
 
-    // si slides[i] === dot[index] donc on rajoute dot_selected
+    /** Checks if the current dot corresponds to the index,
+    * if true it adds the class dot_selected.*/
     if (i === index) {
       dot.classList.add('dot_selected');
     }
   }
 }
-// call the function(PAS OULIER DAPPELER LA FONCTION)
+/** Calls the function.*/
 displayDots()
